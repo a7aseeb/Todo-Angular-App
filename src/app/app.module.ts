4aa70590b4b62742606737a865cb3806318c1646
Abdul Haseeb <ahaseeb0809@gmail.com>
2021-09-21 10:27:35 +0530

    Todo Angular App

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoListComponent } from './components/to-do-list/to-do-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/to-do-list/to-do-list.component.ts
@@ -0,0 +1,132 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../interfaces/todo';
+import { trigger, transition, style, animate } from '@angular/animations';
+
+@Component({
+  selector: 'to-do-list',
+  templateUrl: './to-do-list.component.html',
+  styleUrls: ['./to-do-list.component.scss'],
+  animations: [
+    trigger('fade', [
+
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(30px)' }),
+        animate(200, style({ opacity: 1, transform: 'translateY(0px)'}))
+      ]),
+
+      transition(':leave', [
+        animate(200, style({ opacity: 0, transform: 'translateY(30px)' }))
+      ]),
+
+    ])
+  ]
+})
+export class TodoListComponent implements OnInit {
+  todos: Todo[] = [];
+  todoTitle!: string;
+  idForTodo!: number;
+  beforeEditCache!: string;
+  filter!: string;
+  anyRemainingModel!: boolean;
+  // event!: number;
+
+
+  constructor() { }
+
+  ngOnInit() {
+    this.anyRemainingModel = true;
+    this.filter = 'all';
+    this.beforeEditCache = '';
+    this.idForTodo = 4;
+    this.todoTitle = '';
+    this.todos = [];    
+  }
+
+  addTodo(): void {
+    if (this.todoTitle.trim().length === 0) {
+      return;
+    }
+
+    this.todos.push({
+      id: this.idForTodo,
+      title: this.todoTitle,
+      completed: false,
+      editing: false
+    })
+
+    this.todoTitle = '';
+    this.idForTodo++;
+  }
+
+  editTodo(todo: Todo): void {
+    this.beforeEditCache = todo.title;
+    todo.editing = true;
+  }
+
+  doneEdit(todo: Todo): void {
+    if (todo.title.trim().length === 0) {
+      todo.title = this.beforeEditCache;
+    }
+
+    this.anyRemainingModel = this.anyRemaining();
+    todo.editing = false;
+  }
+
+  cancelEdit(todo: Todo): void {
+    todo.title = this.beforeEditCache;
+    todo.editing = false;
+  }
+
+  deleteTodo(id: number): void {
+    this.todos = this.todos.filter(todo => todo.id !== id);
+  }
+
+  remaining(): number {
+    return this.todos.filter(todo => !todo.completed).length;
+  }
+
+  atLeastOneCompleted(): boolean {
+    return this.todos.filter(todo => todo.completed).length > 0;
+  }
+
+  clearCompleted(): void {
+    this.todos = this.todos.filter(todo => !todo.completed);
+  }
+
+  // checkAllTodos(event: { target: any; }): void {
+  //   this.todos.forEach(todo => todo.completed = (event.target).checked)
+  //   this.anyRemainingModel = this.anyRemaining();
+  // }
+
+  anyRemaining(): boolean {
+    return this.remaining() !== 0;
+  }
+
+  todosFiltered(): Todo[] {
+    if (this.filter === 'all') {
+      return this.todos;
+    } else if (this.filter === 'active') {
+      return this.todos.filter(todo => !todo.completed);
+    } else if (this.filter === 'completed') {
+      return this.todos.filter(todo => todo.completed);
+    }
+
+    return this.todos;
+  }
+
+  
+
+  getLocalTodos(): void{
+    if(localStorage.getItem('todos')== null){
+      localStorage.setItem('todos',JSON.stringify([]));
+    } else {
+      //  var todoLocal = JSON.parse(localStorage.getItem('todos'));
+      //  this.addTodo();
+    }
+
+  }
+}
+
+
+
+
